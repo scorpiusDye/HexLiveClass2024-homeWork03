2c1fb94f84f1c51903db1000d6a27c1e8ac795f5
scorpiusDye <166988307+scorpiusDye@users.noreply.github.com>
2024-08-18 11:14:28 +0800

    任務七-aos.js載入設定調整

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 import "./assets/scss/all.scss";
-import "./assets/aos.js";
-// import 'aos/dist/aos.js';
+import AOS from "aos";
+import "aos/dist/aos.js";
 
-//   document.addEventListener("DOMContentLoaded", function () {
-//     const desktopWidth = 1200;
+document.addEventListener("DOMContentLoaded", function () {
+  const desktopWidth = 1200;
 
-//     // 如果是桌面設備，初始化 AOS
-//     if (window.innerWidth >= desktopWidth) {
-//       AOS.init({
-//         once: true, // 只在首次滾動時觸發動畫
-//       });
-//     } else {
-//       // 如果是移動設備，移除所有 data-aos 屬性，確保元素顯示
-//       const aosElements = document.querySelectorAll("[data-aos]");
-//       aosElements.forEach(function (element) {
-//         element.removeAttribute("data-aos");
-//         element.style.opacity = 1; // 確保元素不透明
-//         element.style.transform = "none"; // 移除動畫效果
-//       });
-//     }
-//   });
+  // 如果是桌面設備，初始化 AOS
+  if (window.innerWidth >= desktopWidth) {
+    AOS.init({
+      once: true, // 只在首次滾動時觸發動畫
+    });
+  } else {
+    // 如果是移動設備，移除所有 data-aos 屬性，確保元素顯示
+    const aosElements = document.querySelectorAll("[data-aos]");
+    aosElements.forEach(function (element) {
+      element.removeAttribute("data-aos");
+      element.style.opacity = 1; // 確保元素不透明
+      element.style.transform = "none"; // 移除動畫效果
+    });
+  }
+});
